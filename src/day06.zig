6d5e9c0028fa07392a768e3fe1a2db82867b4879
Basile Henry <bjm.henry@gmail.com>
2021-11-12 23:47:45 +0100

    Fix stack overflow in ReleaseSafe by allocating grid to heap

--- src/day06.zig
+++ src/day06.zig
@@ -4,34 +4,55 @@ const print = std.debug.print;
 const Pos = struct { x: usize, y: usize };
 
 const Grid = struct {
-    grid: [1000][1000]bool,
+    allocator: *std.mem.Allocator,
+    grid: *[1000][1000]bool,
 
-    pub fn on(self: *Grid, low: Pos, high: Pos) void {
+    pub fn init(allocator: *std.mem.Allocator) !Grid {
+        var grid = try allocator.create([1000][1000]bool);
+
+        for (grid) |*row| {
+            for (row) |*cell| {
+                cell.* = false;
+            }
+        }
+
+        return Grid{
+            .allocator = allocator,
+            .grid = grid,
+        };
+    }
+
+    pub fn deinit(self: *Grid) void {
+        self.allocator.free(self.grid);
+        self.* = undefined;
+    }
+
+    pub fn on(self: Grid, low: Pos, high: Pos) void {
         var x = low.x;
         while (x <= high.x) : (x += 1) {
             var y = low.y;
             while (y <= high.y) : (y += 1) {
-                self.*.grid[y][x] = true;
+                self.grid[y][x] = true;
             }
         }
     }
 
-    pub fn off(self: *Grid, low: Pos, high: Pos) void {
+    pub fn off(self: Grid, low: Pos, high: Pos) void {
         var x = low.x;
         while (x <= high.x) : (x += 1) {
             var y = low.y;
             while (y <= high.y) : (y += 1) {
-                self.*.grid[y][x] = false;
+                self.grid[y][x] = false;
             }
         }
     }
 
-    pub fn toggle(self: *Grid, low: Pos, high: Pos) void {
+    pub fn toggle(self: Grid, low: Pos, high: Pos) void {
         var x = low.x;
         while (x <= high.x) : (x += 1) {
             var y = low.y;
             while (y <= high.y) : (y += 1) {
-                self.*.grid[y][x] = !self.grid[y][x];
+                self.grid[y][x] = !self.grid[y][x];
             }
         }
     }
@@ -53,36 +74,57 @@ const Grid = struct {
 };
 
 const Grid2 = struct {
-    grid: [1000][1000]usize,
+    allocator: *std.mem.Allocator,
+    grid: *[1000][1000]usize,
 
-    pub fn on(self: *Grid2, low: Pos, high: Pos) void {
+    pub fn init(allocator: *std.mem.Allocator) !Grid2 {
+        var grid = try allocator.create([1000][1000]usize);
+
+        for (grid) |*row| {
+            for (row) |*cell| {
+                cell.* = 0;
+            }
+        }
+
+        return Grid2{
+            .allocator = allocator,
+            .grid = grid,
+        };
+    }
+
+    pub fn deinit(self: *Grid2) void {
+        self.allocator.free(self.grid);
+        self.* = undefined;
+    }
+
+    pub fn on(self: Grid2, low: Pos, high: Pos) void {
         var x = low.x;
         while (x <= high.x) : (x += 1) {
             var y = low.y;
             while (y <= high.y) : (y += 1) {
-                self.*.grid[y][x] += 1;
+                self.grid[y][x] += 1;
             }
         }
     }
 
-    pub fn off(self: *Grid2, low: Pos, high: Pos) void {
+    pub fn off(self: Grid2, low: Pos, high: Pos) void {
         var x = low.x;
         while (x <= high.x) : (x += 1) {
             var y = low.y;
             while (y <= high.y) : (y += 1) {
-                if (self.*.grid[y][x] > 0) {
-                    self.*.grid[y][x] -= 1;
+                if (self.grid[y][x] > 0) {
+                    self.grid[y][x] -= 1;
                 }
             }
         }
     }
 
-    pub fn toggle(self: *Grid2, low: Pos, high: Pos) void {
+    pub fn toggle(self: Grid2, low: Pos, high: Pos) void {
         var x = low.x;
         while (x <= high.x) : (x += 1) {
             var y = low.y;
             while (y <= high.y) : (y += 1) {
-                self.*.grid[y][x] += 2;
+                self.grid[y][x] += 2;
             }
         }
     }
@@ -115,16 +157,17 @@ pub fn main() anyerror!void {
     var line = std.ArrayList(u8).init(allocator);
     defer line.deinit();
 
-    const falses = [_]bool{false} ** 1000;
-    var grid = Grid{ .grid = [_][1000]bool{falses} ** 1000 };
-    const zeroes = [_]usize{0} ** 1000;
-    var grid2 = Grid2{ .grid = [_][1000]usize{zeroes} ** 1000 };
+    var grid = try Grid.init(allocator);
+    defer grid.deinit();
+
+    var grid2 = try Grid2.init(allocator);
+    defer grid2.deinit();
 
     while (reader.readUntilDelimiterArrayList(&line, '\n', 100)) {
         var tokens = std.mem.tokenize(u8, line.items, " ");
 
-        var f: fn (*Grid, Pos, Pos) void = undefined;
-        var g: fn (*Grid2, Pos, Pos) void = undefined;
+        var f: fn (Grid, Pos, Pos) void = undefined;
+        var g: fn (Grid2, Pos, Pos) void = undefined;
 
         var t = tokens.next().?;
         if (std.mem.eql(u8, t, "toggle")) {
@@ -161,8 +204,8 @@ pub fn main() anyerror!void {
             end.y = try std.fmt.parseInt(usize, it.next().?, 10);
         }
 
-        f(&grid, start, end);
-        g(&grid2, start, end);
+        f(grid, start, end);
+        g(grid2, start, end);
     } else |err| {
         if (err != error.EndOfStream) {
             return err;
